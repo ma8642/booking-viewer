077d31a4339d28b2b9be3311beb7a96800785ee2
Marley Alford <marleycalford@gmail.com>
2023-03-27 12:28:43 +0200

    Fixed formatting

--- src/App.vue
+++ src/App.vue
@@ -48,11 +48,7 @@ export default {
 </script>
 
 <template>
-  <header>
-    <div class="wrapper"></div>
-  </header>
-
-  <main>
+  <main id="main-container">
     <input type="text" v-model="search" />
     <div v-for="booking in filteredBookings" :key="booking.id">
       <BookingItem :data="booking"></BookingItem>
@@ -61,30 +57,11 @@ export default {
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 @media (min-width: 1024px) {
-  header {
+  #main-container {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
   }
 }
 </style>
--- src/components/BookingItem.vue
+++ src/components/BookingItem.vue
@@ -1,9 +1,9 @@
 <template>
-  <article>
+  <div className="booking">
     <h2>{{ data.referenceNumber }}</h2>
     <div>{{ data.startTime }} - {{ data.endTime }}</div>
     <div>{{ data.bookingState }}</div>
-  </article>
+  </div>
 </template>
 
 <script>
@@ -11,3 +11,14 @@ export default {
   props: ['data']
 }
 </script>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .booking {
+    border: 1px solid white;
+    border-radius: 10px;
+    padding: 10px 20px;
+    margin: 1em;
+  }
+}
+</style>
