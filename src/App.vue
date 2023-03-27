c1ac68e2a369584327e4717c81799bfb49bce39c
Marley Alford <marleycalford@gmail.com>
2023-03-27 12:02:28 +0200

    Initial commit - showing mock data

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<script>
+import BookingItem from './components/BookingItem.vue'
+export default {
+  components: { BookingItem },
+  data() {
+    return {
+      bookingItems: [
+        {
+          id: 123,
+          referenceNumber: 'foo1',
+          startTime: '1pm',
+          endTime: '2pm',
+          bookingState: 'active'
+        },
+        {
+          id: 456,
+          referenceNumber: 'bar2',
+          startTime: '3pm',
+          endTime: '4pm',
+          bookingState: 'registered'
+        },
+        {
+          id: 789,
+          referenceNumber: 'Baz3',
+          startTime: '12pm',
+          endTime: '1pm',
+          bookingState: 'completed'
+        },
+        {
+          id: 101,
+          referenceNumber: 'bom4',
+          startTime: '2pm',
+          endTime: '3pm',
+          bookingState: 'active'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper"></div>
+  </header>
+
+  <main>
+    <div v-for="booking in bookingItems" :key="booking.id">
+      <BookingItem :data="booking"></BookingItem>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/BookingItem.vue
@@ -0,0 +1,13 @@
+<template>
+  <article>
+    <h2>{{ data.referenceNumber }}</h2>
+    <div>{{ data.startTime }} - {{ data.endTime }}</div>
+    <div>{{ data.bookingState }}</div>
+  </article>
+</template>
+
+<script>
+export default {
+  props: ['data']
+}
+</script>
